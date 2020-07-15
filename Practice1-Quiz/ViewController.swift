0a20b7d1476a47b8483f6b37bab64a7e4676f4a9
herjared <herjaredsinai@gmail.com>
2020-07-15 14:16:33 -0400

    Initial Commit

---
+++ Practice1-Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice1-Quiz
+//
+//  Created by Sina on 7/15/20.
+//  Copyright © 2020 Sina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
